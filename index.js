bf1d61bb741d87a17db88884ee6e1b4fa632ed33
Samuel Amity <sam.amity@gmail.com>
2022-09-12 14:19:13 -0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
+// OR
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    const seenNumbers = {}
+    for (const number of array) {
+      const complement = target - number;
+      if (complement in seenNumbers) return true;
+      seenNumbers[number] = true;
+    }
+    return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
+runtime: 0(n^2)
+space: 0(n)
+
+OR
+runtime: 0(n)
+space: 0(n+1)
+
 
 /* 
-  Add your pseudocode here
+  hasTargetSum ([22, 19, 4, 6, 30], 25)
+  iterate through each number in the array
+    for the current number, identify a completement that adds to the target (complement = target - number)
+    iterate through the rest of the array
+      check if any number of the complement
+      if so, return true
+
+    if I reach end of the array without complement found, return false
+
+OR
+
+  create an object to keep track of numbers we have already seen
+  iterate through each number in the array
+    for the current number, identify the complement whose sum will equal the target
+    check if any key on our object is the complement
+      if so return true
+      otherwise, add that number to the object
 */
 
 /*
